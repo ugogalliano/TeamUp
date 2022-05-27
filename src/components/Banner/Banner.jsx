76fff07ca2b579a424bea4a0ca6eca901a574253
ugo.galliano <galliano@threads.it>
2022-05-27 13:40:32 +0200

    commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,9 +8,13 @@ const Banner = () => {
 
             <div className={classes.banner__container}>
 
+              
+
                 <div className={classes.banner__img}>
-                    <img src={teamUpSolution} alt="banner_team_up" style={{ maxWidth: "100%", height: "100%" }} />
-                </div>
+                     <img src={teamUpSolution} alt="banner_team_up" style={{ maxWidth: "100%", height: "100%" }} />
+ 
+
+                 </div>
 
                 <div className={classes.banner__content}>
                     <div className={classes.banner__title}>
@@ -19,7 +23,7 @@ const Banner = () => {
                     <div className={classes.banner__subtitle}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus pretium quam vulputate dignissim suspendisse in.
                     </div>
-                </div>
+                </div> 
             </div>
 
 
